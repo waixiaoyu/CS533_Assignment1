296cb8f563e3665ccb7925df980bda7220c8f97d
waixiaoyu <yaoyu_best@163.com>
2017-02-12 00:20:35 -0500

    0212

--- taggingIntro.py
+++ taggingIntro.py
@@ -7,11 +7,15 @@ print nltk.pos_tag(text)
 text = nltk.word_tokenize("the permit")
 print nltk.pos_tag(text)
 
-#document for each tag
+# document for each tag
 nltk.help.upenn_tagset('RB')
 
 text = nltk.word_tokenize("They refuse to permit us to obtain the refuse permit")
 print nltk.pos_tag(text)
 
+print
 brown_news_tagged = brown.tagged_words(categories='news',tagset='universal')
-print brown_news_tagged[:15]
+print brown_news_tagged[:30]
+
+tag_fd = nltk.FreqDist(tag for (word, tag) in brown_news_tagged)
+print tag_fd.most_common()
