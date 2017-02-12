5d66c051b6ddc2838d7f9154edc4f42d91bd9829
waixiaoyu <yaoyu_best@163.com>
2017-02-12 11:24:36 -0500

    0212

--- taggingIntro.py
+++ taggingIntro.py
@@ -2,20 +2,27 @@ import nltk
 from nltk.corpus import brown
 
 text = nltk.word_tokenize("they permit")
-print nltk.pos_tag(text)
+nltk.pos_tag(text)
 
 text = nltk.word_tokenize("the permit")
-print nltk.pos_tag(text)
+nltk.pos_tag(text)
 
 # document for each tag
 nltk.help.upenn_tagset('RB')
 
 text = nltk.word_tokenize("They refuse to permit us to obtain the refuse permit")
-print nltk.pos_tag(text)
+nltk.pos_tag(text)
 
-print
 brown_news_tagged = brown.tagged_words(categories='news',tagset='universal')
-print brown_news_tagged[:30]
+brown_news_tagged[:30]
 
 tag_fd = nltk.FreqDist(tag for (word, tag) in brown_news_tagged)
-print tag_fd.most_common()
+tag_fd.most_common()
+
+word_tag_fd = nltk.FreqDist(brown_news_tagged)
+def common_items(target_tag) :
+    return [ word for ((word,tag), count) in
+            word_tag_fd.most_common()[:1000]
+            if tag == target_tag ]
+
+print common_items('ADJ')
